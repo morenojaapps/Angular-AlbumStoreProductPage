d21708fc2e31d83c4e7f34c162fcc797cf0d7e86
JAIME MORENO MSI <jamoreno528@gmail.com>
2020-06-20 17:53:26 -0500

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -12,14 +12,10 @@ export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
 
-  constructor(
-   private _productService:ProductService,
-    ) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(
-      response => this.albumInfo = response
-    )
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
