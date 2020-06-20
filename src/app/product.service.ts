88624a1aead6ff03728fc24a65c53e7ccbb7bb7c
JAIME MORENO MSI <jamoreno528@gmail.com>
2020-06-20 17:19:47 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -12,8 +14,9 @@ export class ProductService {
     private _http: Http
   ) { }
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id:number):Observable<Album>
+  {
+    return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 
 }
---
+++ src/app/track.ts
@@ -0,0 +1,6 @@
+export interface Track {
+    trackNumber:number;
+    trackName:string;
+    tranckLength:string;
+    trackPrice:number;
+}
